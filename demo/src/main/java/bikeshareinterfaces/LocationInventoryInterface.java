ecdc46f3ffbde3c6e302460b693f13ee5ba9d1b1
Viresh Jivane <viresh.jivane@sjsu.edu>
2014-11-15 18:31:27 -0800

    Interface for Location Inventory.

--- demo/src/main/java/bikeshareinterfaces/LocationInventoryInterface.java
+++ demo/src/main/java/bikeshareinterfaces/LocationInventoryInterface.java
@@ -1,1 +1,16 @@
+package bikeshareinterfaces;
 
+import java.text.ParseException;
+import java.util.*;
+
+public interface LocationInventoryInterface {
+	
+	public String[] getInvForAnHour(int location_id, Date date, int hour ) throws ParseException;
+	
+	public Map<Integer, String[]> getInvForAnHourAtVariousLoc(int[] arrayOfLocation_id, Date date, int hour );
+	
+	public Map<Integer, String[]> getInvForSeveralHoursAtOneLocation(int location_id, Date date, int[] arrayOfHours );
+			
+	public void updateInvForBookingHours(int location_id, Date date, int[] arrayOfHours, String bikeID );
+	
+}
